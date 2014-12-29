70ce383efc4e769a703345958ce12d19b945e2d9
Will Hastings <will@objectdotcreate.net>
2014-12-28 17:24:46 -0800

    Set up Resource model.

--- models/resource.js
+++ models/resource.js
@@ -3,11 +3,25 @@
 import mongoose from 'mongoose';
 
 var resourceSchema,
-    Resource;
+    Resource,
+    Schema = mongoose.Schema,
+    ObjectId = Schema.Types.ObjectId;
 
 resourceSchema = mongoose.Schema({
-  title: String,
-  url: String
+  title: {type: String, required: true},
+  domain: {type: String, required: true},
+  path: String,
+  queryString: String,
+  port: {type: Number, required: true, default: 80},
+  protocol: {
+    type: String, required: true, default: 'http',
+    enum: {values: ['http', 'https'], message: 'protocol is invalid'}
+  },
+  description: String,
+  user: {type: ObjectId, required: true, index: true},
+  category: {type: ObjectId, required: true, index: true},
+  createdAt: {type: Date, default: Date.now, index: true},
+  updatedAt: {type: Date, default: Date.now}
 });
 
 Resource = mongoose.model('Resource', resourceSchema);
